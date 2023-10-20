85f0670a103f85732e6ac756b473b55e2e49aefe
Wilfred <wilfredmutwiri20@gmail.com>
2023-10-20 17:06:12 +0300

    Add nav mobile responsiveness section

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
-// // alert("Hello")
-// let priceHolder=document.getElementById("priceHolder");
-// priceHolder.addEventListener("mouseover",()=>{
-//     alert("hello there!")
-// })
+let menu=document.querySelector("#menu");
+let nav=document.querySelector("#nav");
+let navHeight=document.querySelector("#navHeight");
+menu.addEventListener("mouseenter",()=>{
+    if(nav.classList.contains("invisible")){
+        nav.classList.remove("invisible");
+    }else
+    {
+        nav.classList.add("invisible");
+    }
+})
